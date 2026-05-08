5c2628545bf80bffae561938377e68e6f0ece93b
Lucas <Lucasdel45@gmail.com>
2026-05-08 14:01:44 -0300

    Criadas pastas e configurações iniciais

---
+++ backend/src/main/java/com/galinho/backend/exception/EntidadeNaoEncontradaException.java
@@ -0,0 +1,7 @@
+package com.galinho.backend.exception;
+
+public class EntidadeNaoEncontradaException extends RuntimeException {
+    public EntidadeNaoEncontradaException(String msg) {
+        super(msg);
+    }
+}
