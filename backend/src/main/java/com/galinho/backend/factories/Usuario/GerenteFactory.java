34c5b9419cc991139bf2a05f9ff16ecab1833500
Leticia Leal <leticialleal28@gmail.com>
2026-06-13 17:55:29 -0300

    Mudando factories de usuários

--- backend/src/main/java/com/galinho/backend/factories/Usuario/GerenteFactory.java
+++ backend/src/main/java/com/galinho/backend/factories/Usuario/GerenteFactory.java
@@ -1,14 +1,19 @@
 package com.galinho.backend.factories.Usuario;
 
+import java.time.LocalDate;
+
 import org.springframework.stereotype.Component;
 
 import com.galinho.backend.model.Usuarios.Gerente;
+import com.galinho.backend.utils.Role;
 
 @Component
 public class GerenteFactory implements UsuarioFactory {
     
     public Gerente criarUsuario() {
         Gerente usuario = new Gerente();
+        usuario.setDataCadastro(LocalDate.now());
+        usuario.setRole(Role.GERENTE);
         return usuario;
     }
 }
