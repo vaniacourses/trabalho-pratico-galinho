be83863066c7438bcd76f71b420720134acca84d
Camille Sá <camille.perazzini@gmail.com>
2026-06-14 01:55:44 -0300

    Change page title to 'Alteração de Servico'

--- frontend/src/pages/AlterarServicoPage.tsx
+++ frontend/src/pages/AlterarServicoPage.tsx
@@ -4,7 +4,7 @@ import ServicoForm from "../components/ServicoForm"
 const AlterarServicoPage = () => {
   return (
     <>
-      <h1 className="mb-1 text-xl font-semibold">Cadastro de Servico</h1>
+      <h1 className="mb-1 text-xl font-semibold">Alteração de Servico</h1>
       <hr className="mb-4" />
 
       <ServicoForm />
